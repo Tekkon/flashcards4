2ce26ef6843949ac8ef67712a272a004029da5f1
Nikita <ningen.tos@gmail.com>
2016-05-29 20:33:21 +0500

    поправил end of file

--- app/controllers/admin_controller.rb
+++
@@ -1,9 +0,0 @@
-class AdminController < ApplicationController
-  def show
-    if AdminPolicy.new(current_user).show?
-      render :template => "admin/index"
-    else
-      redirect_to root_path
-    end
-  end
-end
